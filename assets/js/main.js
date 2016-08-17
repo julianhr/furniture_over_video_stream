c3372a41f250ae7011cb99fc4f70f258d1df35f7
Julian Hernandez <julian.hernandez@magmalabs.io>
2016-08-17 15:59:34 -0500

    code refactored for efficiency on the client side

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,158 +1,195 @@
 'use strict';
 
-// VARIABLES
-var 
-  canvas = document.querySelector('#canvas'),
-  video = document.querySelector('#stream'),
-  wrapper = document.querySelector('#user-ui'),
-  windowWidth = window.innerWidth,
-  windowHeight = window.innerHeight,
-  videoLeftOffset = 0,
-  videoTopOffset = 0,
-  stream,
+function furnitureOnVideo() {
+  // VARIABLES
+  var 
+    wrapper = document.querySelector('div#user-ui'),
+    canvas = document.querySelector('canvas#canvas'),
+    imgCanvas = document.querySelector('canvas#composite'),
+    video = document.querySelector('video#stream'),
+    videoLeftOffset = 0,
+    videoTopOffset = 0,
 
-  imgCapture = document.querySelector('#capture'),
-  btnCapture = document.querySelector('#btn-capture'),
-  aSave = document.querySelector('#btn-save'),
-  btnAgain = document.querySelector('#btn-again'),
+    imgCapture = document.querySelector('img#capture'),
+    btnCapture = document.querySelector('button#btn-capture'),
+    aSave = document.querySelector('a#btn-save'),
+    btnAgain = document.querySelector('button#btn-again'),
 
-  fabricCanvas = new fabric.Canvas(canvas),
-  fabricItem = new fabric.Image(document.querySelector('#item'));
+    fabricCanvas = new fabric.Canvas(canvas),
+    fabricItem = new fabric.Image(document.querySelector('#item'));
 
 
-// SET VIDEO
-function getVideoStream() {
-  var constraints = function() {
-    if (stream) {
-      stream.getTracks().forEach(function(track) {
-        track.stop();
-      });
+  // FUNCTIONS
+  function uiButtons(state) {
+    if (state === 'capture') {
+      btnCapture.style.display = 'inline';
+      aSave.style.display = 'none';
+      btnAgain.style.display = 'none';
+    } else if (state === 'save') {
+      btnCapture.style.display = 'none';
+      aSave.style.display = 'inline';
+      btnAgain.style.display = 'inline';
+    }
+  }
+
+  function getVideoStream() {
+    var constraints = {
+      video: true, 
+      audio: false,
+      facingMode: { exact: "environment" }
+    };
+
+    var successCallback = function(mediaStream) {
+      window.stream = mediaStream;
+      video.srcObject = mediaStream;
     }
 
-    return {video: true, audio: false};
-  }();
+    var errorCallback = function(error) {
+      var errorName = 'getUserMedia name: ' + error.name;
 
-  var successCallback = function(mediaStream) {
-    window.stream = mediaStream;
-    video.srcObject = mediaStream;
+      console.log(errorName);
+      console.log(error);
+    }
+
+    // execute getUserMedia Promise
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(successCallback)
+      .catch(errorCallback);
+  }
+
+  function setInitialState() {
+    imgCapture.style.display = 'none';
+    imgCapture.src = "";
+    uiButtons('capture');
+  }
+
+  function resizeWrapper() {
+    wrapper.style.width = window.innerWidth + "px";
+    wrapper.style.height = window.innerHeight + "px";
+  }
+
+  function resizeVideoTag() {
+    var 
+      videoRatioWidthToHeight = video.videoWidth / video.videoHeight,
+      windowRatioWidthToHeight = window.innerWidth / window.innerHeight,
+      videoWidth = 0,
+      videoHeight = 0,
+      px = "px",
+      minus = "-";
+
+    // window aspect is narrower than video
+    if (videoRatioWidthToHeight < windowRatioWidthToHeight) {
+        var heightDiff = Math.round(window.innerWidth / videoRatioWidthToHeight) - window.innerHeight;
+
+        videoWidth = window.innerWidth;
+        videoHeight = Math.round(window.innerWidth / videoRatioWidthToHeight);
+
+        if (heightDiff > 0) {
+          videoTopOffset = Math.round(heightDiff / 2);
+        }
+    // window aspect is wider than video
+    } else if (videoRatioWidthToHeight > windowRatioWidthToHeight) {
+        var widthDiff = Math.round(window.innerHeight * videoRatioWidthToHeight) - window.innerWidth;
+
+        videoHeight = window.innerHeight;
+        videoWidth = Math.round(window.innerHeight * videoRatioWidthToHeight);
+
+        if (widthDiff > 0) {
+          videoLeftOffset = Math.round(widthDiff / 2);
+        }
+    // window has same aspect as video
+    } else {
+      videoWidth = window.innerWidth;
+      videoHeight = window.innerHeight;
+    }
+
+    video.width = videoWidth;
+    video.height = videoHeight;
+    video.style.width = videoWidth + px;
+    video.style.height = videoHeight + px;
+    video.style.marginTop = minus + videoTopOffset + px;
+    video.style.marginLeft = minus + videoLeftOffset + px;
+  }
+
+  function resizeFabricCanvas() {
+    fabricCanvas.setWidth(window.innerWidth);
+    fabricCanvas.setHeight(window.innerHeight);
+  }
+
+  function resizeimgCanvasCanvas() {
+    imgCanvas.width = window.innerWidth;
+    imgCanvas.height = window.innerHeight;
+  }
+
+  function setFabricItem() {
+    var coord = fabricCanvas.getCenter();
+
+    fabricItem.set({
+      originX: 'center',
+      originY: 'center',
+      top: coord.top + 50,
+      left: coord.left,
+      borderColor: 'green',
+      borderDashArray: [10, 10],
+      cornerStyle: 'circle',
+      cornerSize: 44,
+      borderScaleFactor: .3,
+      cornerColor: 'orange',
+      lockUniScaling: true
+    });
+
+    fabricCanvas
+      .add(fabricItem)
+      .setActiveObject(fabricItem)
+      .renderAll();
+  }
+
+  function main() {
+    getVideoStream();
+    setInitialState();
+    resizeWrapper();
+    resizeFabricCanvas();
+    resizeimgCanvasCanvas();
+    setFabricItem();
+
+    video.onloadedmetadata = function() {
+      resizeVideoTag();
+    };
+  }
+  main();
+
+
+  // EVENT LISTENERS
+  btnCapture.onclick = function(event) {
+    video.pause();
+
+    fabricItem.set({selectable: false});
+    fabricCanvas.deactivateAll().renderAll();
+    
+    uiButtons('save');
+  };
+
+  aSave.onclick = function(event) {
+    var ctx = imgCanvas.getContext('2d');
+
+    ctx.drawImage(video, -videoLeftOffset, -videoTopOffset, video.width, video.height);
+    ctx.drawImage(canvas, 0, 0);
+
+    // set image for download
+    aSave.href = imgCanvas.toDataURL();
+    aSave.download = "alameda_visualizacion.png"
+  }
+
+  btnAgain.onclick = function(event) {
     video.play();
 
-    console.log("Video stream from the mobile device successfully obtained.");
-  }
+    fabricItem.set({selectable: true});
+    fabricCanvas.setActiveObject(fabricItem).renderAll();
 
-  var errorCallback = function(error) {
-    var errorName = 'getUserMedia name: ' + error.name;
-
-    console.log(errorName);
-    console.log(error);
-  }
-
-  // execute getUserMedia Promise
-  navigator.mediaDevices.getUserMedia(constraints)
-    .then(successCallback)
-    .catch(errorCallback);
-}
-getVideoStream();
-
-
-function resizeVideoTag() {
-  var 
-    videoRatioWidthToHeight = video.videoWidth / video.videoHeight,
-    windowRatioWidthToHeiht = windowWidth / windowHeight,
-    orientation = (windowWidth / windowHeight >= 1) ? 'horizontal' : 'vertical';
-
-  // window aspect is narrower than video
-  if (videoRatioWidthToHeight < windowRatioWidthToHeiht) {
-      var heightDiff = Math.round(windowWidth / videoRatioWidthToHeight) - windowHeight;
-
-      video.width = windowWidth;
-      video.height = Math.round(windowWidth / videoRatioWidthToHeight);
-
-      if (heightDiff > 0) {
-        videoTopOffset = Math.round(heightDiff / 2);
-        video.style.marginTop = "-" + videoTopOffset + "px";
-      }
-  // window aspect is wider than video
-  } else if (videoRatioWidthToHeight > windowRatioWidthToHeiht) {
-      var widthDiff = Math.round(windowHeight * videoRatioWidthToHeight) - windowWidth;
-
-      video.height = windowHeight;
-      video.width = Math.round(windowHeight * videoRatioWidthToHeight);
-
-      if (widthDiff > 0) {
-        videoLeftOffset = Math.round(widthDiff / 2)
-        video.style.marginLeft = "-" + videoLeftOffset + "px";
-      }
-  // window has same aspect as video
-  } else {
-    video.width = windowWidth;
-    video.height = windowHeight;
-  }
+    imgCapture.src = "";
+    uiButtons('capture');
+  };
 }
 
-function resizeFabricCanvas() {
-  fabricCanvas.setWidth(windowWidth);
-  fabricCanvas.setHeight(windowHeight);
-}
-
-function setFabricItem() {
-  fabricItem.set({top: 50, left: 50});
-
-  fabricCanvas
-    .add(fabricItem)
-    .setActiveObject(fabricItem);
-
-  fabricCanvas.renderAll();
-}
-
-function combineElements() {
-  wrapper.style.width = windowWidth + "px";
-  wrapper.style.height = windowHeight + "px";
-
-  resizeVideoTag();
-  resizeFabricCanvas();
-  setFabricItem();
-}
-
-// combineElements();
-video.onloadedmetadata = combineElements;
-
-function uiButtons(state) {
-  if (state === 'capture') {
-    btnCapture.style.display = 'inline';
-    aSave.style.display = 'none';
-    btnAgain.style.display = 'none';
-  } else if (state === 'save') {
-    btnCapture.style.display = 'none';
-    aSave.style.display = 'inline';
-    btnAgain.style.display = 'inline';
-  }
-}
-
-
-// EVENT LISTENERS
-btnCapture.onclick = function(event) {
-  var 
-    upperCanvas = document.querySelector('.upper-canvas'),
-    ctx = upperCanvas.getContext('2d'),
-    screenshot = new fabric.Image(imgCapture);
-
-  // grab video frame and overlay the furniture
-  fabricCanvas.deactivateAll().renderAll();
-  ctx.drawImage(video, -videoLeftOffset, -videoTopOffset, video.width, video.height);
-  ctx.drawImage(canvas, 0, 0);
-  imgCapture.src = upperCanvas.toDataURL();
-  aSave.href = imgCapture.src;
-  aSave.download = "visualizacion.png"
-  ctx.clearRect(0, 0, windowWidth, windowHeight);
-
-  uiButtons('save');
-  fabricCanvas.add(screenshot);
-  fabricCanvas.renderAll();
-};
-
-btnAgain.onclick = function(event) {
-  imgCapture.src = "";
-  uiButtons('capture');
-  fabricCanvas.setActiveObject(fabricItem);
-};
+window.onload = furnitureOnVideo;
+window.onresize = furnitureOnVideo;
